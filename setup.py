7e75a9464d9bea7d1a66d0fa10c9c29b236d04a9
Sougata P <skall.paul@gmail.com>
2020-04-18 14:13:56 +0530

    Updated sample project & application structure

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mocherry",
-    version="1.0.8",
+    version="1.0.9",
     author="Sougata P.",
     author_email="skall.paul@gmail.com",
     description="CherryPy REST webservice framework with MongoDB ORM support",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/techunits/mocherry",
-    download_url="https://github.com/techunits/mocherry/archive/1.0.8.tar.gz",
+    download_url="https://github.com/techunits/mocherry/archive/1.0.9.tar.gz",
     packages=setuptools.find_packages(),
     zip_safe=True,
     classifiers=[
